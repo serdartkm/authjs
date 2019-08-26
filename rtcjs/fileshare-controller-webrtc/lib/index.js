ed58b5768e86701650e890e61161e607e22f2412
serdartkm <tkm.house.new@gmail.com>
2019-08-26 15:02:50 +0500

    WebRTCFileShare module is complete

--- rtcjs/fileshare-controller-webrtc/lib/index.js
+++ rtcjs/fileshare-controller-webrtc/lib/index.js
@@ -3,19 +3,29 @@ import { sendArrayBuffer,sendString, useDataChannel, rtcStateUpdate, createOffer
 
 
 class WebRTCFileShareController extends React.Component {
-    state = { file: "", currentChunk: 0, incomingFileInfo: "", incomingFileData: [], bytesRecieved: "", downloadInProgress: false, progress: 0, assembledFile: null, BYTES_PER_CHUNK: 50, start: 0, end: 0 }
+
+    constructor(props){
+
+        super(props)
+
+        this.state = { file: "", currentChunk: 0, incomingFileInfo: "", incomingFileData: [], bytesRecieved: "", downloadInProgress: false, progress: 0, assembledFile: null, BYTES_PER_CHUNK: 50, start: 0, end: 0 }
+        this.downloadRef = React.createRef()
+    }
+
+   
 
 
 
     componentDidMount() {
         const { downloadInProgress } = this.state
-        const { initiator, sendOffer, sendCandidate } = this.props
+        const { initiator, sendOffer, sendCandidate} = this.props
   
 
 
         this.fileReader = new FileReader()
 
         this.fileReader.onloadend = (r) => {
+            const {bytesRecieved,incomingFileInfo }= this.state
             if (r.target.readyState == FileReader.DONE) {
             var buffer = r.target.result
             const { file,currentChunk,BYTES_PER_CHUNK } = this.state
@@ -25,19 +35,23 @@ class WebRTCFileShareController extends React.Component {
                 if(buffer.byteLength>0){
               
                     sendArrayBuffer({ self: this, message: r.target.result })
-               
+                    
                 }
-          
-
-            if (Number.parseInt((BYTES_PER_CHUNK * currentChunk)) < Number.parseInt( file.size) ) {
+           //console.log("BYTES_PER_CHUNK * currentChunk+++=+++",BYTES_PER_CHUNK * currentChunk)
+           //console.log("file size",file.size)
+            if (Number.parseInt((BYTES_PER_CHUNK * currentChunk)) <= Number.parseInt( file.size) ) {
 
             //    console.log("BYTES_PER_CHUNK??????????????",BYTES_PER_CHUNK)
              //   console.log("currentChunk????????????",currentChunk)
              //   console.log("file.size????????????",BYTES_PER_CHUNK * currentChunk,file.size)
              //   console.log("BYTES_PER_CHUNK * currentChunk < file.size",BYTES_PER_CHUNK * currentChunk < file.size)
-       
+
+    
                 this.readNextChunk(file);
-         
+                if (bytesRecieved === incomingFileInfo.fileSize) {
+                    console.log("downlaod ended!!!!!!!!!!!!!!!!!!!--------------")
+                    this.endDownload()
+                }
            
             }
         }}
@@ -45,17 +59,19 @@ class WebRTCFileShareController extends React.Component {
 
         useDataChannel({
             self: this, onMessage: (e) => {
-                console.log("message recived-----", e)
+         
+             //   console.log("message recived-----", e)
               
              //console.log("message--is--", JSON.parse( e.data))
                 if (downloadInProgress === false && typeof(e.data)==="string") {
                     const { message } = JSON.parse( e.data)
-                    console.log('3. Recieved file info',message)
+                //    console.log('3. Recieved file info',message)
                     this.startDownload({ message })
                 }
                 else {
-                   console.log("6. File chunk recieved", e)
-                 //   this.progressDownload(e)
+                 //  console.log("6. File chunk recieved", e)
+                   this.progressDownload(e)
+               
                 }
             },
             sendCandidate
@@ -81,12 +97,12 @@ class WebRTCFileShareController extends React.Component {
     
         if (e.target.files[0] !== null) {
             const file = e.target.files[0]
-             console.log("1.Selecting file...:", file)
+           //  console.log("1.Selecting file...:", file)
             this.setState({ file: e.target.files[0], currentChunk: 0 })
 
             const message = { fileName: file.name, fileSize: file.size, type: "fileinfo" }
 
-            console.log("2.Sending file info...:", message)
+          //  console.log("2.Sending file info...:", message)
             sendString({ self: this, message })
           //  console.log('slice>>>>>>>>>>>', file.slice(0, 100))
             this.readNextChunk(file)
@@ -101,17 +117,21 @@ class WebRTCFileShareController extends React.Component {
     //2.CHUNKING FILE
     //3.READ INTO MEMORY CHANKED DATA
     readNextChunk = (file) => {
-      //  console.log("8.Readnext chunk",file)
+     //   console.log("8.Readnext chunk",file)
         const { currentChunk, BYTES_PER_CHUNK, start, end } = this.state
        // console.log("CURRENT CHUNK", currentChunk)
        // console.log("BYTES_PER_CHUNK", BYTES_PER_CHUNK)
-       // console.log("FILE SIZE", file.size)
+        console.log("FILE SIZE", file.size)
         let startNew = BYTES_PER_CHUNK * currentChunk
+        console.log("start new",startNew)
         let endNew = Math.min(file.size, startNew + BYTES_PER_CHUNK)
-        this.setState({ start: startNew, end: endNew })
-       // console.log('FILE---start,end', start, end)
+        console.log("endNew",endNew)
+        console.log("FIle size:",file.size)
+       // console.log("calculated size:",start + BYTES_PER_CHUNK)
+      //  console.log('FILE---start,end', start, end)
 
-        this.fileReader.readAsArrayBuffer(file.slice(start, end))
+        this.fileReader.readAsArrayBuffer(file.slice(startNew, endNew))
+        this.setState({ start: startNew, end: endNew })
         this.setState((prevState) => ({ currentChunk: ++prevState.currentChunk }))
     }
 
@@ -123,32 +143,43 @@ class WebRTCFileShareController extends React.Component {
     }
 
     progressDownload = (e) => {
-       console.log('7.Process file chunk recieved', e.data)
+    //   console.log('7.Process file chunk recieved', e.data)
         const { bytesRecieved, incomingFileInfo } = this.state
-        let progress = ((bytesRecieved / incomingFileInfo) * 100).toFixed(2)
-        this.setState((prevState) => ({ bytesRecieved: e.data.byteLength, incomingFileData: [...prevState.incomingFileData, e.data], progress }))
-        if (bytesRecieved === incomingFileInfo.fileSize) {
+       // console.log("incomingFileInfo",incomingFileInfo)
+        console.log("bytesRecieved...",bytesRecieved)
+        console.log("e.data.byteLength-----",e.data.byteLength)
+        let progress = (((bytesRecieved +e.data.byteLength )/ incomingFileInfo.fileSize) * 100).toFixed(2)
+        console.log("Total bytes recived", e.data.byteLength +bytesRecieved)
+        this.setState((prevState) => ({ bytesRecieved: e.data.byteLength + prevState.bytesRecieved, incomingFileData: [...prevState.incomingFileData, e.data], progress }))
+        if ((bytesRecieved+e.data.byteLength ) === incomingFileInfo.fileSize) {
+            console.log("downlaod ended!!!!!!!!!!!!!!!!!!!--------------")
             this.endDownload()
         }
-        //  console.log("download progress", progress)
+          console.log("download progress", progress)
 
     }
     endDownload = () => {
 
-        //  const assembledFile = new Blob(this.state)
-        this.setState({ downloadInProgress: false })
+        const {incomingFileData,incomingFileInfo} =this.state
+     //console.log("incomingFileData......", incomingFileData)
+         const assembledFile = new Blob(incomingFileData) 
+        console.log("assembledFile",assembledFile)
+        this.setState({ downloadInProgress: false,assembledFile })
 
-    }
+        this.downloadRef.current.href=URL.createObjectURL( assembledFile );
+        this.downloadRef.current.download= incomingFileInfo.fileName
+    }   
   
-
-  
-
     render() {
         const { file, assembledFile } = this.state
 
 
         const { children } = this.props
-        return (<div>{children({ ...this.props, ...this.state, file, onFileChange: this.onFileChange, assembledFile })}</div>)
+        return (<div>{children({ ...this.props, ...this.state, file, onFileChange: this.onFileChange, assembledFile })}
+        <div style={{height:50,width:"100%",backgroundColor:"yellow"}}>
+            <a ref={this.downloadRef}>Download recived file</a>
+        </div>
+        </div>)
     }
 
 }
