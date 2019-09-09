e1e60ae996a654fee5e9be5a2eadff69f8f485a0
serdartkm <tkm.house.new@gmail.com>
2019-09-09 15:45:35 +0500

    s

--- rtcjs/rtc-demo/client/Home.js
+++ rtcjs/rtc-demo/client/Home.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import MessagingModule from '@rtcjs/messaging-module'
-import io from "socket.io-client";
-const  firstUser = io("http://localhost:3000/", { query: `name=tkm.house.new@gmail.com` });
-const secondUser = io("http://localhost:3000/", { query: `name=tkm.house.old@gmail.com` });
+
+
+
 class Home extends React.Component{
 
     render(){
