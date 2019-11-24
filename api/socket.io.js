1e8d575c6464c4ac5eb1f1fbef206983252f65cf
serdartkm <tkm.house.new@gmail.com>
2019-11-24 20:23:44 +0500

    ccc

--- api/socket.io.js
+++ api/socket.io.js
@@ -1,5 +1,6 @@
-const socketServer =require('../rtcjs/nodejs-socketio-text-chat')
+// const socketServer =require('../rtcjs/nodejs-socketio-text-chat')
 
 module.exports=(req,res)=>{
-socketServer(null,'anonymous')
+    res.json({message:"hello from socket.io"})
+// socketServer(null,'anonymous')
 }
